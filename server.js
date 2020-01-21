23898792e6321bde6175e040068efdcdc7002ef7
Abhishyam <abhishyamnayak@gmail.com>
2020-01-21 23:36:47 +0530

    coorected typo from heroku to heruko in server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+console.log(__dirname);
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/deploy-heroku'));
+app.use(express.static(__dirname + '/dist/deploy-heruko'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join('index.html'));
+res.sendFile(path.join(__dirname + '/dist/deploy-heruko/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
